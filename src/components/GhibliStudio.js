368f88ba67ed88e7d005739387f684651f73298d
Aranguez <leandro-aranguez@hotmail.com>
2019-05-16 21:07:42 -0300

    routes

--- src/components/GhibliStudio.js
+++ src/components/GhibliStudio.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import '../styles/index.scss';
 
 import Nav from './nav'
@@ -37,14 +38,20 @@ class GhibliStudio extends Component {
   
   render() {
     return (
-      <Fragment>
-        <Nav/>
-        <div className="container">
-          { this.state.movies.map( (movie, i) => {
-            return <Card key={i} title={movie.title} description={movie.description}/>
-          })}
-        </div>
-      </Fragment>
+      
+
+      <Router>
+        <Fragment>
+          <Nav/>
+          {/*<Route path="/" exact component={Index} />*/}
+          {/*<Route path="/movie/:id" exact component={Description} />*/}
+          <div className="container">
+            { this.state.movies.map( (movie, i) => {
+              return <Card key={i} title={movie.title} description={movie.description}/>
+            })}
+          </div>
+        </Fragment>
+      </Router>
     )
   };
 }
